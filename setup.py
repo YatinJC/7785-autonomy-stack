34e009c7e8dac01edf474e234ac9139a2dfc423c
Arun-23-09-2003 <125459338+Arun-23-09-2003@users.noreply.github.com>
2025-09-16 16:24:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'rotate_robot = WESUCKATCODING_object_follower.rotate_robot:main'
         ],
     },
 )
