6ad31d3fe96793eb155c8797cbf5a7d5a60479cd
Arun-23-09-2003 <125459338+Arun-23-09-2003@users.noreply.github.com>
2025-09-17 17:01:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'rotate_robot = WESUCKATCODING_object_follower.rotate_robot:main'
-            'dectector = WESUCKATCODING_object_follower.dectector:main'
+            'dectector = WESUCKATCODING_object_follower.dectect:main'
             'viewer = WESUCKATCODING_object_follower.viewer:main'
         ],
     },
